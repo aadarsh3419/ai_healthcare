8e659996f868ee2baccb617aadfd911295d89770
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2026-05-01 00:36:56 +0530

    f

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.core.database import connect_db, disconnect_db
 from app.api.auth import router as auth_router
+from app.api.chat import router as chat_router  # ← ADD KARO
 
 @asynccontextmanager
-async def lifespan(app:FastAPI):#this function is used to manage the lifespan of the application and it will be used in the main.py file to run the application
-
+async def lifespan(app:FastAPI):
     await connect_db()
-
     print(f"starting your health{settings.APP_NAME}...")
     yield
-    
     await disconnect_db()
     print("server shutting down")
 
-app = FastAPI(#this functionn is used to create the fastapi application and it will be used in the main.py file to run the application
+app = FastAPI(
     title = settings.APP_NAME,
     debug=settings.DEBUG,
     lifespan=lifespan
 )
 
-app.add_middleware(#this function is used to add the cors middleware to the application and it will be used in the main.py file to run the application
+app.add_middleware(
     CORSMiddleware,
     allow_origins = ["http://localhost:3000"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
 )
-app.include_router(auth_router, prefix="/api/auth", tags=["auth"])#this function is used to include the auth router in the application and it will be used in the main.py file to run the application
 
-@app.get("/")#this function is used to create the root endpoint for the application and it will be used in the main.py file to run the application 
-async def root():#this function is used  to crate the root endpoint for the application and it will be used in the main,py file to run the application 
+app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
+app.include_router(chat_router, prefix="/api/chat", tags=["chat"])  # ← ADD KARO
+
+@app.get("/")
+async def root():
     return {"message":f"{settings.APP_NAME} is running"}
